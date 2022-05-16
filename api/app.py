76fee53b7e3c271e33a3a356677f75457965801b
josemelendezCR <jose.melendez.respaldo@gmail.com>
2022-05-15 21:44:57 -0600

    Added SWAGGER documentation

--- api/app.py
+++ api/app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 from routes.teams_routes import router
 from utils.db import db
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 CORS(app)
+
+# Configure SWAGGER
+@app.route('/static/<path:path>')
+def send_static(path):
+  return send_from_directory('static', path)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Team Role API"
+    }
+)
+
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
 
 # Development env database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost:5001/react-flask-posgresql'
